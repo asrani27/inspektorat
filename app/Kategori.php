8daa4d9abe6595f418f56f1f5d1f8d086011ecb5
asranim <asrani.27@gmail.com>
2019-12-08 15:55:41 +0800

    fix

--- app/Kategori.php
+++ app/Kategori.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Kategori extends Model
 {
-    protected $table = 'Kategori';
+    protected $table = 'kategori';
 
     public function filewbk()
     {
